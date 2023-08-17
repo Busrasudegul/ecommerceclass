9edaecb449f340cc5882488e69ddcac346c6d826
busrasudegul <busrasudeg@gmail.com>
2023-08-17 12:27:32 +0300

    14.08

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,1 +1,29 @@
 //// burada google firebase ile etkileşime buradan girilecek.
+
+
+import { initializeApp } from "firebase/app";
+
+//authentication işlemleri için giriş ve yetkilendirme
+import {getAuth} from "firebase/auth"
+
+//firestore database erişimi için (verilerin kayıt yeri için)
+import {getFirestore} from "firebase/firestore"
+
+// storage erişimi için (resim kayıt yeri)
+import {getStorage} from "firebase/storage"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC18lV1Q2K7R55V2s716mtChTIvd7yB7lM",
+  authDomain: "ecommerceclass-1a2b1.firebaseapp.com",
+  projectId: "ecommerceclass-1a2b1",
+  storageBucket: "ecommerceclass-1a2b1.appspot.com",
+  messagingSenderId: "439376949570",
+  appId: "1:439376949570:web:e432ac605b38efb77a98db"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
+export default app
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,9 +1,65 @@
 //// kayıtlı olmayan kulllanıcılar için kayıt sayfası
-import React from 'react'
+import React, { useState, useNavigate } from 'react';
+import styles from "./auth.module.scss";
+import Card from '../../components/card/Card';
+import { Link } from 'react-router-dom';
+import registerImg from "../../assets/register.png";
+import { toast } from "react-toastify";
+import { auth } from "../../firebase/config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import Loader from "../../components/loader/Loader"
 
 const Register = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [cPassword, setcPassword] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+
+  const navigate = useNavigate()
+
+  const registerUser = (e) => {
+    e.preventDefault()
+    // console.log(email,password,cPassword)
+    if (password !== cPassword) {
+      toast.error("Password do not match")
+    } else {
+      setIsLoading(true)
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user)
+          setIsLoading(false)
+          toast.success("Registration successfull..")
+          navigate("/login")
+        })
+        .catch((error) => {
+          setIsLoading(false)
+          toast.error(error.message)
+        });
+    }
+  }
   return (
-    <div>Register</div>
+    <>
+      {isLoading && <Loader/>}
+      <section className={`container ${styles.auth}`}>
+        <Card cardClass={styles.form}>
+          <h2>Register</h2>
+          <form onSubmit={registerUser}>
+            <input type="text" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)}></input>
+            <input type="password" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)}></input>
+            <input type="password" placeholder="Confirm Password" required value={cPassword} onChange={(e) => setcPassword(e.target.value)}></input>
+            <button className="--btn --btn-primary --btn-block">Register</button>
+          </form>
+          <span className={styles.register}>
+            <p>Already have an account ? &nbsp; </p>
+            <Link to="/login">Login</Link>
+          </span>
+        </Card>
+        <div className={styles.img}>
+          <img src={registerImg} alt="register" width="400"></img>
+        </div>
+      </section>
+    </>
   )
 }
 
