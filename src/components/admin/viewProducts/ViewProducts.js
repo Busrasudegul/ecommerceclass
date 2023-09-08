c9466f4ed84110f92f42ff2f9a20a6b7b278749a
busrasudegul <busrasudeg@gmail.com>
2023-09-08 17:39:05 -0400

    25.08

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -9,43 +9,23 @@ import { Link } from 'react-router-dom'
 import { FaEdit, FaTrashAlt } from 'react-icons/fa'
 import { deleteObject, ref } from 'firebase/storage'
 import Notiflix from 'notiflix'
-import { useDispatch } from 'react-redux'
-import { STORE_PRODUCTS } from '../../../redux/slice/productSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { STORE_PRODUCTS, selectProducts } from '../../../redux/slice/productSlice'
+import useFetchCollection from '../../../customHooks/useFetchCollection'
 
 const ViewProducts = () => {
 
-  const [products, setProducts] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const {data,isLoading} = useFetchCollection("products")
+  const products = useSelector(selectProducts)
 
   const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(STORE_PRODUCTS({products:data}))
+  },[dispatch,data])
 
-  useEffect(() => {
-    getProducts()
-  }, [])
+  
 
-  const getProducts = () => {
-    setIsLoading(true)
-    try {
-      const productRef = collection(db, "products")
-      const q = query(productRef, orderBy("createdAt", "desc"))
-      onSnapshot(q, (snapshot) => {
-        // console.log(snapshot)
-        // console.log(snapshot.docs[0].data())
-        const allProducts = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data()
-        }))
-        //console.log(allProducts)
-        setProducts(allProducts)
-        setIsLoading(false)
-        dispatch(STORE_PRODUCTS({products:allProducts}))
-      })
-    }
-    catch (error) {
-      setIsLoading(false)
-      toast.error(error.message)
-    }
-  }
+
 
   const deleteProduct = async(id,imageURL) => {
     try {
--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -1,9 +1,55 @@
 //// home sayfasındaki ürünler hakkında gösterim, arama, sıralama yapabildiğiniz kısımla beraber tüm ürün kartlarının sergilendiği yer. ürün kartları (productıtem) adlı komponentde işlenecektir.
-import React from 'react'
+import React, { useState } from 'react'
+import styles from "./ProductList.module.scss"
+import { FaListAlt } from 'react-icons/fa'
+import {BsFillGridFill} from "react-icons/bs"
+import Search from '../../search/Search'
+import ProductItem from '../productItem/ProductItem'
 
-const ProductList = () => {
+
+const ProductList = ({products}) => {
+  const [grid,setGrid] = useState(true)
+  const [search,setSearch] = useState("")
   return (
-    <div>ProductList</div>
+    <div className={styles["product-list"]} id="product" >
+      <div className={styles.top}>
+        <div className={styles.icon}>
+          <BsFillGridFill size={22} color="orangered" onClick={()=>setGrid(true)}/>
+          <FaListAlt size={24} color="#0066d4" onClick={()=>setGrid(false)}/>
+          <p>
+            <b>10</b>Products Found
+          </p>
+        </div>
+        <div>
+          <Search value={search} onChange={(e)=>setSearch(e.target.value)}/>
+        </div>
+        <div className={styles.sort}>
+          <label>Sort by:</label>
+          <select name="category">
+            <option value="latest">Latest</option>
+            <option value="lowest-price">Lowest Price</option>
+            <option value="highest-price">Highest Price</option>
+            <option value="a-z">a-z</option>
+            <option value="z-a">z-a</option>
+          </select>
+        </div>
+      </div>
+      <div className={grid ? `${styles.grid}` : `${styles.list}`}>
+        {products.length === 0 ? (
+          <p>No product found.</p>
+        ) : (
+          <>
+          {products.map((product)=>{
+            return (
+              <div key={product.id}>
+              <ProductItem {...product} grid={grid} product={product}/>
+              </div>
+            )
+          })}
+          </>
+        )}
+      </div>
+    </div>
   )
 }
 
